ee85b0d77b646e3a67685191e3e3da0e4c3c991e
JulianGarbate <juliangarbate@gmail.com>
2026-05-27 09:06:41 -0300

    Initial commit

---
+++ Assets/scripts/playerRecolectation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerRecolectation : MonoBehaviour
+{
+    public int cantidadRecolectada = 0;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("recolectable"))
+        {
+            cantidadRecolectada++;
+            Destroy(col.gameObject);
+        }
+    }
+}
