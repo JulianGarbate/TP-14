d1a369ecef6afddbb05217a8e5750ed33edd48f2
JulianGarbate <juliangarbate@gmail.com>
2026-06-03 09:05:53 -0300

    logica al gamemanager sume manzana y la hice low polly falta afinar el pasillo y hacer low polly el palpel

--- Assets/scripts/UiManager.cs
+++ Assets/scripts/UiManager.cs
@@ -7,14 +7,31 @@ public class UiManager : MonoBehaviour
 {
     public TextMeshProUGUI textoScore;
     public TextMeshProUGUI textoTimer;
-
+    public bool gano = false;
     public void SumarPunto(int puntaje)
     {
             textoScore.text = "Puntos: " + puntaje.ToString();
+            if(puntaje == 0){
+                textoScore.text = "Ganaste";
+                gano = true;
+            }
     }
 
     public void UpdateTimer(float tiempoRestante)
-    {        
-            textoTimer.text = "Tiempo: " + tiempoRestante.ToString("F1");
+    {       
+            if(gano == false && tiempoRestante != 0)
+            {
+                textoTimer.text = "Tiempo: " + tiempoRestante.ToString("F1");
+            }
+            else if(gano == false && tiempoRestante == 0){
+                textoTimer.text = "";
+                textoScore.text = "Perdiste";   
+            }
+            else
+            {
+                textoTimer.text = "";
+            }
+
     }
+
 }
