662b17887d0d6ed8a3a81ce5f04b910eb658bf46
JulianGarbate <juliangarbate@gmail.com>
2026-06-19 09:16:02 -0300

    AVANCES

---
+++ Assets/scripts/linternaManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class linternaManager : MonoBehaviour
+{
+    public Light linterna;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            linterna.enabled = !linterna.enabled;
+        }
+    }
+}
